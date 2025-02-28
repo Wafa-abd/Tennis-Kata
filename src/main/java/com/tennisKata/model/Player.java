a1fc2748611dd5c1a403eba8edf1ae11bc74e592
W BESSROUR <bessrour.wafa14@gmail.com>
2025-02-28 14:59:00 +0100

    [tennis-service]: tennis match implementation

---
+++ src/main/java/com/tennisKata/model/Player.java
@@ -0,0 +1,32 @@
+package com.tennisKata.model;
+
+public class Player {
+    private String code;
+    private String name;
+
+    public Player(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
